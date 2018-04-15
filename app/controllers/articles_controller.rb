505dc4bc4b0c462966a5b0d9b5aacc41c7c73713
ARUN KUMAR <arunpasupathi24@gmail.com>
2018-04-15 19:48:55 +0530

    Added Secure Password mechanism

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        
+        @article.user = User.first
         if @article.save
             flash[:notice] = "The Article is successfully saved"
             redirect_to article_path(@article)
